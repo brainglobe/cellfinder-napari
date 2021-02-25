aa3bcc968450666b8a114d21f456d187dddb6ddb
Adam Tyson <adam.tyson@ucl.ac.uk>
2021-02-25 18:11:09 +0000

    Update to magic_factory

--- cellfinder_napari/cellfinder_napari.py
+++ cellfinder_napari/cellfinder_napari.py
@@ -1,74 +1,49 @@
+import napari
+from magicgui import magic_factory
 from napari_plugin_engine import napari_hook_implementation
-from cellfinder_core.main import main as cellfinder_run
 
-from magicgui import magicgui
+from cellfinder_core.main import main as cellfinder_run
 
 from .utils import cells_to_array
 
 
-def widget_wrapper():
-    @magicgui(
-        signal=dict(label="Signal image"),
-        background=dict(label="Background image"),
-        voxel_size_z=dict(
-            widget_type="FloatSpinBox",
-            label="Voxel size (z)",
-            min=0.0,
-            max=100.0,
-            step=0.1,
-            value=5.0,
-        ),
-        voxel_size_y=dict(
-            widget_type="FloatSpinBox",
-            label="Voxel size (z)",
-            min=0.0,
-            max=100.0,
-            step=0.1,
-            value=2.0,
-        ),
-        voxel_size_x=dict(
-            widget_type="FloatSpinBox",
-            label="Voxel size (z)",
-            min=0.0,
-            max=100.0,
-            step=0.1,
-            value=2.0,
-        ),
-        call_button=True,
+@magic_factory(
+    voxel_size_z=dict(label="Voxel size (z)", step=0.1),
+    voxel_size_y=dict(label="Voxel size (y)", step=0.1),
+    voxel_size_x=dict(label="Voxel size (x)", step=0.1),
+    call_button=True,
+)
+def cellfinder(
+    signal: "napari.layers.Image",
+    background: "napari.layers.Image",
+    voxel_size_z: float = 5,
+    voxel_size_y: float = 2,
+    voxel_size_x: float = 2,
+) -> napari.types.LayerDataTuple:
+
+    voxel_sizes = (voxel_size_z, voxel_size_y, voxel_size_x)
+    points = cellfinder_run(
+        signal.data,
+        background.data,
+        voxel_sizes,
+        start_plane=0,
+        end_plane=-1,
+        n_free_cpus=2,
     )
-    def widget(
-        signal: "napari.layers.Image",
-        background: "napari.layers.Image",
-        voxel_size_z: float,
-        voxel_size_y: float,
-        voxel_size_x: float,
-    ) -> "napari.types.LayerDataTuple":
 
-        voxel_sizes = (voxel_size_z, voxel_size_y, voxel_size_x)
-        points = cellfinder_run(
-            signal.data,
-            background.data,
-            voxel_sizes,
-            start_plane=0,
-            end_plane=-1,
-            n_free_cpus=2,
-        )
+    points = cells_to_array(points)
 
-        points = cells_to_array(points)
-
-        properties = {
-            "name": "Points",
-            "size": 15,
-            "n_dimensional": True,
-            "opacity": 0.6,
-            "symbol": "ring",
-            "face_color": "lightgoldenrodyellow",
-        }
-        return points, properties, "points"
-
-    return widget
+    properties = {
+        "name": "Points",
+        "size": 15,
+        "n_dimensional": True,
+        "opacity": 0.6,
+        "symbol": "ring",
+        "face_color": "lightgoldenrodyellow",
+    }
+    return points, properties, "points"
 
 
 @napari_hook_implementation
 def napari_experimental_provide_dock_widget():
-    return widget_wrapper, {"name": "cellfinder"}
+    return cellfinder
