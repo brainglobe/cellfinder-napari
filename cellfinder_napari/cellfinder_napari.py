5803d065e522f4f4c6d9ba34ef87ad2edc62ec50
Adam Tyson <adam.tyson@ucl.ac.uk>
2021-02-26 13:02:16 +0000

    add more params to gui

--- cellfinder_napari/cellfinder_napari.py
+++ cellfinder_napari/cellfinder_napari.py
@@ -11,24 +11,55 @@ from .utils import cells_to_array
     voxel_size_z=dict(label="Voxel size (z)", step=0.1),
     voxel_size_y=dict(label="Voxel size (y)", step=0.1),
     voxel_size_x=dict(label="Voxel size (x)", step=0.1),
+    ball_xy_size=dict(label="Ball filter (xy)"),
+    ball_z_size=dict(label="Ball filter (z)"),
+    Soma_diameter=dict(step=0.1),
+    Ball_overlap=dict(step=0.1),
+    Filter_width=dict(step=0.1),
+    Cell_spread=dict(step=0.1),
+    Classification_batch_size=dict(max=4096),
     call_button=True,
 )
 def cellfinder(
-    signal: "napari.layers.Image",
-    background: "napari.layers.Image",
+    Signal_image: "napari.layers.Image",
+    Background_image: "napari.layers.Image",
     voxel_size_z: float = 5,
     voxel_size_y: float = 2,
     voxel_size_x: float = 2,
+    Soma_diameter: float = 16.0,
+    ball_xy_size: int = 6,
+    ball_z_size: int = 15,
+    Ball_overlap: float = 0.6,
+    Filter_width: float = 0.2,
+    Threshold: int = 10,
+    Cell_spread: float = 1.4,
+    Max_cluster: int = 100000,
+    Start_plane: int = 0,
+    End_plane: int = 0,
+    Classification_batch_size: int = 32,
+    Number_of_free_cpus: int = 2,
 ) -> napari.types.LayerDataTuple:
+
+    if End_plane == 0:
+        End_plane = len(Signal_image.data)
 
     voxel_sizes = (voxel_size_z, voxel_size_y, voxel_size_x)
     points = cellfinder_run(
-        signal.data,
-        background.data,
+        Signal_image.data,
+        Background_image.data,
         voxel_sizes,
-        start_plane=0,
-        end_plane=-1,
-        n_free_cpus=2,
+        soma_diameter=Soma_diameter,
+        ball_xy_size=ball_xy_size,
+        ball_z_size=ball_z_size,
+        start_plane=Start_plane,
+        end_plane=End_plane,
+        ball_overlap_fraction=Ball_overlap,
+        log_sigma_size=Filter_width,
+        n_sds_above_mean_thresh=Threshold,
+        soma_spread_factor=Cell_spread,
+        max_cluster_size=Max_cluster,
+        n_free_cpus=Number_of_free_cpus,
+        batch_size=Classification_batch_size,
     )
 
     points = cells_to_array(points)
