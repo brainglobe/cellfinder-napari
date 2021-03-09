fcc32914bfbb1ea9600d13f89b634f7e1a3fb1d3
Adam Tyson <adam.tyson@ucl.ac.uk>
2021-03-09 17:50:23 +0000

    remov eunused code

--- cellfinder_napari/plugins.py
+++ cellfinder_napari/plugins.py
@@ -3,10 +3,6 @@ from cellfinder_napari.detect import detect
 from napari_plugin_engine import napari_hook_implementation
 
 
-def napari_experimental_provide_dock_widget():
-    return detect, {"name": "Cell detection"}
-
-
 @napari_hook_implementation
 def napari_experimental_provide_dock_widget():
     return [(detect, {"name": "Cell detection"}), (train, {"name": "Train network"})]
