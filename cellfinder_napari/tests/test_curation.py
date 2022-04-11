94e0fb0990e4ff31b0bd5e443569529d621d16c4
David Stansby <dstansby@gmail.com>
2022-04-11 15:34:51 +0100

    Convert comment to docstring in test
    Co-authored-by: Patrick Roddy <patrickjamesroddy@gmail.com>

--- cellfinder_napari/tests/test_curation.py
+++ cellfinder_napari/tests/test_curation.py
@@ -3,7 +3,9 @@ from cellfinder_napari.curation import CurationWidget
 
 
 def test_detect_function(make_napari_viewer):
-    # Smoke test that adding the curation widget to napari works
+    """
+    Smoke test that adding the curation widget to napari works
+    """
     viewer = make_napari_viewer()
     widget = CurationWidget(viewer)
     viewer.window.add_dock_widget(widget)
