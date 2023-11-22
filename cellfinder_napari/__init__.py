67662079ee8b977bdc0192dc8b9238461003f468
willGraham01 <1willgraham@gmail.com>
2023-11-22 13:53:44 +0000

    Add deprecation warning on import

--- cellfinder_napari/__init__.py
+++ cellfinder_napari/__init__.py
@@ -1,3 +1,10 @@
+from warnings import warn
+
+warn(
+    f"cellfinder-napari has merged with it's backend code and is now available as a combined package. To remain up to date, please install cellfinder: https://github.com/brainglobe/cellfinder",
+    DeprecationWarning,
+)
+
 __version__ = "0.0.20"
 __author__ = "Adam Tyson"
 __license__ = "GPL-3.0"
